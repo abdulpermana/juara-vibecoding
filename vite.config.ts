99a6a367f929b8a8eddeb23ac0112736ea514002
abdulpermana <abdulpermana.dev@gmail.com>
2026-05-06 14:21:53 +0700

    Update GEMINI_API_KEY definition in vite.config.ts
    Replace loading of GEMINI_API_KEY from environment with a hardcoded value.

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyBODmgk2G0RwAjoiSNWDVwLQ80zpQKAz_k"),
     },
     resolve: {
       alias: {
